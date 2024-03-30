df3625d9623c726eb87f46a66c07283a5048b81e
Mihail Iurco <mihai.iurcom@gmail.com>
2024-03-30 15:41:45 +0000

    Disable noice

--- nvim/lua/plugins/to_config.lua
+++ nvim/lua/plugins/to_config.lua
@@ -5,7 +5,7 @@ vim.opt.termguicolors=true
 require('plugins/config/bamboo')
 require('plugins/config/cmp')
 require('plugins/config/gitsigns')
-require('plugins/config/noice')
+-- require('plugins/config/noice')
 require('plugins/config/nvim-tree')
 require('plugins/config/telescope')
 require('plugins/config/which-key')
