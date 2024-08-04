680cf19e4e900591bd752691e13200573622313e
Mihail Iurco <mihai.iurcom@gmail.com>
2024-08-04 15:19:08 +0100

    Expand or shring selections

--- nvim/lua/plugins/config/treesitter.lua
+++ nvim/lua/plugins/config/treesitter.lua
@@ -35,9 +35,9 @@ require 'nvim-treesitter.configs'.setup {
 		enable = true,
 		keymaps = {
 			init_selection = "gnn", -- set to `false` to disable one of the mappings
-			node_incremental = "grn",
+			node_incremental = ">",
 			scope_incremental = "grc",
-			node_decremental = "grm",
+			node_decremental = "<",
 		},
 	},
 	textobjects = { enable = true },
