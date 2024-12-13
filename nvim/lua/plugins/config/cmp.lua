aa8dc1be6d7e520be26afc2396e7b0aa0961241e
Mihail Iurco <mihai.iurcom@gmail.com>
2024-12-13 09:32:53 +0000

    Tidy, remove vsnip

--- nvim/lua/plugins/config/cmp.lua
+++ nvim/lua/plugins/config/cmp.lua
@@ -1,44 +1,20 @@
 local cmp = require('cmp')
 
--- Completion
-local feedkey = function(key, mode)
-	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
-end
-local has_words_before = function()
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 cmp.setup({
 	window = {
 		completion = cmp.config.window.bordered(),
 		documentation = cmp.config.window.bordered(),
 	},
-	mapping = cmp.mapping.preset.insert({
-		["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-		["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-		['<Tab>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif vim.fn["vsnip#available"](1) == 1 then
-				feedkey("<Plug>(vsnip-expand-or-jump)", "")
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
-			end
-		end, { "i", "s" }),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-	}),
+	mapping = cmp.mapping.preset.insert({}),
+
 	sources = cmp.config.sources(
 		{
-			{ name = 'nvim_lsp' },
-			{ name = 'vsnip' }, -- For vsnip users.
-			{ name = 'path' },
+			{ name = 'nvim_lsp' }, -- Primary source
 		},
 		{
-			{ name = 'buffer' },
-		}),
+			{ name = 'buffer' }, -- Fallback source: Buffer words if Lsp has no suggestion
+		}
+	),
 })
 
 
