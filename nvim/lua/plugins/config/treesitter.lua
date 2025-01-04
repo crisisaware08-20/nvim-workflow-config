ad71c1f1a96a96b5b12d3f09d3f757da6ffef4f9
Mihail Iurco <mihail.iurco@sainsburys.co.uk>
2025-01-04 15:13:36 +0000

    disable mappings that interfer with existing workflow

--- nvim/lua/plugins/config/treesitter.lua
+++ nvim/lua/plugins/config/treesitter.lua
@@ -6,7 +6,7 @@ require 'nvim-treesitter.configs'.setup {
 	-- parser_install_dir = "/some/path/to/store/parsers",
 
 	-- A list of parser names, or "all"
-	ensure_installed = { "c", "lua", "rust", "java" },
+	ensure_installed = { "c", "lua", "rust" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
@@ -44,116 +44,116 @@ require 'nvim-treesitter.configs'.setup {
 	indent = { enable = true }
 }
 
-require 'nvim-treesitter.configs'.setup {
-	textobjects = {
-		move = {
-			enable = true,
-			set_jumps = true, -- whether to set jumps in the jumplist
-			goto_next_start = {
-				["np"] = "@parameter.inner",
-				["nc"] = "@call.outer",
-				["nic"] = "@call.inner",
-				["nf"] = "@function.outer",
-				["nif"] = "@function.inner",
-				["nb"] = "@conditional.inner",
-				["nl"] = "@loop.inner",
-				["n]"] = "@block.outer",
-				["]m"] = "@function.outer",
-
-				-- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
-				["]o"] = "@loop.*",
-				-- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
-				--
-				-- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
-				-- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
-				["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
-				["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
-			},
-			goto_next_end = {},
-			goto_previous_start = {
-				["Np"] = "@parameter.inner",
-				["Nc"] = "@call.outer",
-				["Nic"] = "@call.inner",
-				["Nf"] = "@function.outer",
-				["Nif"] = "@function.inner",
-				["Nb"] = "@conditional.inner",
-				["Nl"] = "@loop.inner",
-				["N]"] = "@block.outer",
-
-			},
-			goto_previous_end = {},
-			-- Below will go to either the start or the end, whichever is closer.
-			-- Use if you want more granular movements
-			-- Make it even more gradual by adding multiple queries and regex.
-			goto_next = {
-			},
-			goto_previous = {
-			}
-		},
-		swap = {
-			enable = true,
-			swap_next = {
-				["<leader>a"] = "@parameter.inner",
-			},
-			swap_previous = {
-				["<leader>A"] = "@parameter.inner",
-			},
-		},
-		select = {
-			enable = true,
-
-			-- Automatically jump forward to textobj, similar to targets.vim
-			lookahead = true,
-
-			keymaps = {
-				-- You can use the capture groups defined in textobjects.scm
-
-				-- vim.keymap.set('n', 'nc', '<cmd>TSTextobjectGotoNextStart @call.outer<CR>', { desc = 'next call outer' })
-				["ip"] = "@parameter.inner",
-				["p"] = "@parameter.outer",
-				["ic"] = "@call.inner",
-				["c"] = "@call.outer",
-
-				["ib"] = "@conditional.inner",
-				["b"] = "@conditional.outer",
-
-				["f"] = "@function.outer",
-				["if"] = "@function.inner",
-				["l"] = "@loop.outer",
-				["il"] = "@loop.inner",
-				["]"] = "@block.outer",
-				-- You can optionally set descriptions to the mappings (used in the desc parameter of
-				-- nvim_buf_set_keymap) which plugins like which-key display
-				-- ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-				-- You can also use captures from other query groups like `locals.scm`
-				["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-			},
-			-- You can choose the select mode (default is charwise 'v')
-			--
-			-- Can also be a function which gets passed a table with the keys
-			-- * query_string: eg '@function.inner'
-			-- * method: eg 'v' or 'o'
-			-- and should return the mode ('v', 'V', or '<c-v>') or a table
-			-- mapping query_strings to modes.
-			selection_modes = {
-				['@parameter.outer'] = 'v', -- charwise
-				['@function.outer'] = 'V', -- linewise
-				-- ['@class.outer'] = '<c-v>', -- blockwise
-				['@class.outer'] = 'V', -- linewise
-			},
-			-- If you set this to `true` (default is `false`) then any textobject is
-			-- extended to include preceding or succeeding whitespace. Succeeding
-			-- whitespace has priority in order to act similarly to eg the built-in
-			-- `ap`.
-			--
-			-- Can also be a function which gets passed a table with the keys
-			-- * query_string: eg '@function.inner'
-			-- * selection_mode: eg 'v'
-			-- and should return true or false
-			include_surrounding_whitespace = false,
-		},
-	},
-}
+-- require 'nvim-treesitter.configs'.setup {
+-- 	textobjects = {
+-- 		move = {
+-- 			enable = true,
+-- 			set_jumps = true, -- whether to set jumps in the jumplist
+-- 			goto_next_start = {
+-- 				["np"] = "@parameter.inner",
+-- 				["nc"] = "@call.outer",
+-- 				["nic"] = "@call.inner",
+-- 				["nf"] = "@function.outer",
+-- 				["nif"] = "@function.inner",
+-- 				["nb"] = "@conditional.inner",
+-- 				["nl"] = "@loop.inner",
+-- 				["n]"] = "@block.outer",
+-- 				["]m"] = "@function.outer",
+--
+-- 				-- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+-- 				["]o"] = "@loop.*",
+-- 				-- ["]o"] = { query = { "@loop.inner", "@loop.outer" } }
+-- 				--
+-- 				-- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+-- 				-- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+-- 				["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+-- 				["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
+-- 			},
+-- 			goto_next_end = {},
+-- 			goto_previous_start = {
+-- 				["Np"] = "@parameter.inner",
+-- 				["Nc"] = "@call.outer",
+-- 				["Nic"] = "@call.inner",
+-- 				["Nf"] = "@function.outer",
+-- 				["Nif"] = "@function.inner",
+-- 				["Nb"] = "@conditional.inner",
+-- 				["Nl"] = "@loop.inner",
+-- 				["N]"] = "@block.outer",
+--
+-- 			},
+-- 			goto_previous_end = {},
+-- 			-- Below will go to either the start or the end, whichever is closer.
+-- 			-- Use if you want more granular movements
+-- 			-- Make it even more gradual by adding multiple queries and regex.
+-- 			goto_next = {
+-- 			},
+-- 			goto_previous = {
+-- 			}
+-- 		},
+-- 		swap = {
+-- 			enable = true,
+-- 			swap_next = {
+-- 				["<leader>a"] = "@parameter.inner",
+-- 			},
+-- 			swap_previous = {
+-- 				["<leader>A"] = "@parameter.inner",
+-- 			},
+-- 		},
+-- 		select = {
+-- 			enable = true,
+--
+-- 			-- Automatically jump forward to textobj, similar to targets.vim
+-- 			lookahead = true,
+--
+-- 			keymaps = {
+-- 				-- You can use the capture groups defined in textobjects.scm
+--
+-- 				-- vim.keymap.set('n', 'nc', '<cmd>TSTextobjectGotoNextStart @call.outer<CR>', { desc = 'next call outer' })
+-- 				["ip"] = "@parameter.inner",
+-- 				["p"] = "@parameter.outer",
+-- 				["ic"] = "@call.inner",
+-- 				["c"] = "@call.outer",
+--
+-- 				["ib"] = "@conditional.inner",
+-- 				["b"] = "@conditional.outer",
+--
+-- 				["f"] = "@function.outer",
+-- 				["if"] = "@function.inner",
+-- 				["l"] = "@loop.outer",
+-- 				["il"] = "@loop.inner",
+-- 				["]"] = "@block.outer",
+-- 				-- You can optionally set descriptions to the mappings (used in the desc parameter of
+-- 				-- nvim_buf_set_keymap) which plugins like which-key display
+-- 				-- ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+-- 				-- You can also use captures from other query groups like `locals.scm`
+-- 				["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+-- 			},
+-- 			-- You can choose the select mode (default is charwise 'v')
+-- 			--
+-- 			-- Can also be a function which gets passed a table with the keys
+-- 			-- * query_string: eg '@function.inner'
+-- 			-- * method: eg 'v' or 'o'
+-- 			-- and should return the mode ('v', 'V', or '<c-v>') or a table
+-- 			-- mapping query_strings to modes.
+-- 			selection_modes = {
+-- 				['@parameter.outer'] = 'v', -- charwise
+-- 				['@function.outer'] = 'V', -- linewise
+-- 				-- ['@class.outer'] = '<c-v>', -- blockwise
+-- 				['@class.outer'] = 'V', -- linewise
+-- 			},
+-- 			-- If you set this to `true` (default is `false`) then any textobject is
+-- 			-- extended to include preceding or succeeding whitespace. Succeeding
+-- 			-- whitespace has priority in order to act similarly to eg the built-in
+-- 			-- `ap`.
+-- 			--
+-- 			-- Can also be a function which gets passed a table with the keys
+-- 			-- * query_string: eg '@function.inner'
+-- 			-- * selection_mode: eg 'v'
+-- 			-- and should return true or false
+-- 			include_surrounding_whitespace = false,
+-- 		},
+-- 	},
+-- }
 
 local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
 
