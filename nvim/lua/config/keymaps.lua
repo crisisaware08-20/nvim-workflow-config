88ad504dabc7f41f8fd1f6013f458ceeb4be9ea8
Mihail Iurco <mihail@ikigaidigital.io>
2025-10-13 07:37:19 +0100

    tmux and nvim

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,26 @@
+-- Tmux-aware navigation: Ctrl+h/j/k/l to navigate between splits and tmux panes
+-- If at edge of vim splits, pass through to tmux
+local function navigate(direction)
+  local win_before = vim.fn.winnr()
+  vim.cmd('wincmd ' .. direction)
+  local win_after = vim.fn.winnr()
+  
+  -- If we didn't move (at edge), send command to tmux
+  if win_before == win_after then
+    local tmux_direction = ({
+      h = 'L',
+      j = 'D', 
+      k = 'U',
+      l = 'R'
+    })[direction]
+    vim.fn.system('tmux select-pane -' .. tmux_direction)
+  end
+end
+
+vim.keymap.set('n', '<C-h>', function() navigate('h') end, { desc = 'Navigate left', silent = true })
+vim.keymap.set('n', '<C-j>', function() navigate('j') end, { desc = 'Navigate down', silent = true })
+vim.keymap.set('n', '<C-k>', function() navigate('k') end, { desc = 'Navigate up', silent = true })
+vim.keymap.set('n', '<C-l>', function() navigate('l') end, { desc = 'Navigate right', silent = true })
 
 vim.keymap.set('n', ']q', function() if not pcall(vim.cmd, 'cnext') then vim.cmd('cfirst') end vim.cmd('normal! zvzz') end, {desc = 'Quickfix next (wrap)'})
 vim.keymap.set('n', '>>', '<Cmd>cnext<CR>',     { desc = 'Quickfix: next' })
